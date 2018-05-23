05c79b34ca779c0bd2dfff15c84dfe126240f58b
Amanda Mitchell <wreedamitchell@gmail.com>
2018-05-22 20:17:09 -0500

    wip

---
+++ javascripts/dom.js
@@ -0,0 +1,24 @@
+const domString = (movieArray) => {
+  let movieString = '';
+  movieArray.forEach((movie) => {
+    movieString += `<div class="col-sm-6 col-md-4">`;
+    movieString += `<div class="thumbnail">`;
+    // movieString += `<img src="..." alt="...">`;
+    movieString += `<div class="caption">`;
+    movieString += `<h3>${movie.original_title}</h3>`;
+    movieString += `<p>${movie.overview}</p>`;
+    movieString += `<p><a href="#" class="btn btn-primary" role="button">Review</a> <a href="#" class="btn btn-default" role="button">Wishlist</a></p>`;
+    movieString += `</div>`;
+    movieString += `</div>`;
+    movieString += `</div>`;
+  });
+  printToDom(movieString);
+};
+
+const printToDom = (stringz) => {
+  $('#movies').html(stringz);
+};
+
+module.exports = {
+  domString,
+};
