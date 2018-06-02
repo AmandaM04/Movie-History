4664b04da2e89f639776e53c3983cf2b977032c6
Amanda Mitchell <wreedamitchell@gmail.com>
2018-06-02 10:23:19 -0500

    delete button working

--- javascripts/fireBaseApi.js
+++ javascripts/fireBaseApi.js
@@ -42,8 +42,24 @@ const getAllMovies = () => {
   });
 };
 
+const deleteMovieFromDb = (movieId) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      method: 'DELETE',
+      url: `${fireBaseConfig.databaseURL}/movies/${movieId}.json`,
+    })
+      .done(() => {
+        resolve();
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
 module.exports = {
   saveMovieToWishlist,
   setConfig,
   getAllMovies,
+  deleteMovieFromDb,
 };
